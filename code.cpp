de5453e44c741656614e2b5e31052cbfdda63e76
AntiLeaf <antileaf@163.com>
2019-07-14 08:08:45 -0700

    QAQ

--- code.cpp
+++ code.cpp
@@ -3,6 +3,12 @@
 #include "tools.hpp"
 #include "main.hpp"
 
+void disp() {
+	for (int i = 0; i < 32; i++)
+		printf(" %d", reg[i]);
+	printf("\n");
+}
+
 int main() {
 	char c;
 	do
@@ -23,16 +29,32 @@ int main() {
 	}
 	for (;;) {
 		// printf("pc = 0x%x\n", pc);
-		// getchar();
-		IF.work_IF();
-		if (if_id.ir == 0x00c68223) {
-			printf("%d\n", reg[10] & 255);
-			break;
-		}
-		ID.work_ID();
-		EX.work_EX();
-		MEM.work_MEM();
+		getchar();
+		locks = 0;
+		printf("%08x %08x %08x %08x", if_id.ir, id_ex.ir, ex_mem.ir, mem_wb.ir);
+		printf("mem_wb.rd = %d pc = 0x%x\n", get_rd(mem_wb.ir), mem_wb.npc - 4);
 		WB.work_WB();
+		printf("ex_mem.rd = %d pc = 0x%x\n", get_rd(ex_mem.ir), ex_mem.npc - 4);
+		MEM.work_MEM();
+		printf("id_ex.rd = %d pc = 0x%x\n", get_rd(id_ex.ir), id_ex.npc - 4);
+		EX.work_EX();
+		printf("if_id.rd = %d pc = 0x%x\n", get_rd(if_id.ir), if_id.npc - 4);
+		if (ID.work_ID())
+			break;
+		IF.work_IF();
+		disp();
+		if (locks == 5) {
+			printf("Fuck you!\n");
+			for (;;);
+		}
 	}
+	WB.work_WB();
+	MEM.work_MEM();
+	EX.work_EX();
+	WB.work_WB();
+	MEM.work_MEM();
+	WB.work_WB();
+	printf("%d\n", reg[10] & 255);
+	// for (;;);
 	return 0;
 }
--- tools.hpp
+++ tools.hpp
@@ -3,159 +3,54 @@
 
 // I/O
 
-int read_8(const unsigned char*);
+extern int read_8(const unsigned char*);
 
-int read_u8(const unsigned char*);
+extern int read_u8(const unsigned char*);
 
-int read_16(const unsigned char*);
+extern int read_16(const unsigned char*);
 
-int read_u16(const unsigned char*);
+extern int read_u16(const unsigned char*);
 
-int read_32(const unsigned char*);
+extern int read_32(const unsigned char*);
 
-void write_8(unsigned char*, int);
+extern void write_8(unsigned char*, int);
 
-void write_16(unsigned char*, int);
+extern void write_16(unsigned char*, int);
 
-void write_32(unsigned char*, int);
-
-// Integer handling
-
-int get_high(int, int);
-
-int get_low(int, int);
-
-int get_range(int, int, int);
-
-int get_bit(int, int);
-
-int get_highest_bit(int, int);
-
-// Operation decoding
-
-int get_opcode(int);
-
-int get_rs1(int);
-
-int get_rs2(int);
-
-int get_funct3(int);
-
-int get_rd(int);
-
-int get_inst_i(int);
-
-int get_inst_s(int);
-
-int get_inst_b(int);
-
-int get_inst_u(int);
-
-int get_inst_j(int);
-
-// I/O
-
-inline int read_8(const unsigned char *s) {
-	return (int)(char)*s;
-}
-
-inline int read_u8(const unsigned char *s) {
-	return (int)*s;
-}
-
-inline int read_16(const unsigned char *s) {
-	return (int)(*s | (int)(char)*(s + 1) << 8);
-}
-
-inline int read_u16(const unsigned char *s) {
-	return (int)(*s | ((unsigned)*(s + 1) << 8));
-}
-
-inline int read_32(const unsigned char *s) {
-
-
-	return (int)(*s | ((int)*(s + 1) << 8) | ((int)*(s + 2) << 16) | ((int)*(s + 3) << 24));
-}
-
-inline void write_8(unsigned char *s, int x) {
-	*s = x & 255;
-}
-
-inline void write_16(unsigned char *s, int x) {
-	*s = x & 255;
-	*(s + 1) = (x >> 8) & 255;
-}
-
-void write_32(unsigned char *s, int x) {
-	*s = x & 255;
-	*(s + 1) = (x >> 8) & 255;
-	*(s + 2) = (x >> 16) & 255;
-	*(s + 3) = (x >> 24) & 255;
-}
+extern void write_32(unsigned char*, int);
 
 // Integer handling
 
-inline int get_high(int x, int l) { // sign extended
-	return x >> l;
-}
+extern int get_high(int, int);
 
-inline int get_low(int x, int r) {
-	return x & ((1 << (r + 1)) - 1);
-}
+extern int get_low(int, int);
 
-inline int get_range(int x, int l, int r) {
-	return get_low(get_high(x, l), r - l);
-}
+extern int get_range(int, int, int);
 
-inline int get_bit(int x, int t) {
-	return (x >> t) & 1;
-}
+extern int get_bit(int, int);
 
-inline int get_highest_bit(int x, int t) {
-	return ((x >> t) & 1) ? -1 : 0;
-}
+extern int get_highest_bit(int, int);
 
 // Operation decoding
 
-inline int get_opcode(int x) {
-	return get_low(x, 6);
-}
+extern int get_opcode(int);
 
-inline int get_rs1(int x) {
-	return get_range(x, 15, 19);
-}
+extern int get_rs1(int);
 
-inline int get_rs2(int x) {
-	return get_range(x, 20, 24);
-}
+extern int get_rs2(int);
 
-inline int get_funct3(int x) {
-	return get_range(x, 12, 14);
-}
+extern int get_funct3(int);
 
-inline int get_rd(int x) {
-	return get_range(x, 7, 11);
-}
+extern int get_rd(int);
 
-inline int get_inst_i(int x) {
-	return get_high(x, 20);
-}
+extern int get_inst_i(int);
 
-inline int get_inst_s(int x) {
-	return (get_high(x, 25) << 5) | get_range(x, 7, 11);
-}
+extern int get_inst_s(int);
 
-inline int get_inst_b(int x) {
-	return (get_range(x, 8, 11) << 1) | (get_range(x, 25, 30) << 5) |
-		(get_bit(x, 7) << 11) | (get_highest_bit(x, 31) << 12);
-}
+extern int get_inst_b(int);
 
-inline int get_inst_u(int x) {
-	return x & -(1 << 12);
-}
+extern int get_inst_u(int);
 
-inline int get_inst_j(int x) {
-	return (get_range(x, 21, 30) << 1) | (get_bit(x, 20) << 11) |
-		(get_range(x, 12, 19) << 12) | (get_highest_bit(x, 31) << 20);
-}
+extern int get_inst_j(int);
+
 #endif
